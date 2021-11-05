67cda5a601ef5a13ae9f35bcbf9f2918520ff231
Yamagi84 <vincent.agueda@outlook.fr>
2021-11-05 17:57:43 +0100

    Test Affichage METAR TAF avec Swip Left/Right

--- app/src/main/java/com/example/metarmac/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/metarmac/ui/notifications/NotificationsFragment.java
@@ -9,16 +9,28 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.metarmac.GlobalApplication;
 import com.example.metarmac.R;
 import com.example.metarmac.databinding.FragmentNotificationsBinding;
+import com.example.metarmac.model.Airport;
+import com.example.metarmac.ui.OnSwipeTouchListener;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+
+    private TextView test;
+
+    private ArrayList<Airport> lstAirport;
+    private int airportIterator;
+    private Airport actualAirport;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,14 +40,42 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        test = (TextView) root.findViewById(R.id.text_test);
+
+        lstAirport = ((GlobalApplication)getActivity().getApplication()).getLstAirport();
+        airportIterator = 0;
+        actualAirport = lstAirport.get(airportIterator);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        test.setText("N°" + airportIterator + " : " + lstAirport.get(airportIterator).getSite());
+
+        view.setOnTouchListener(new OnSwipeTouchListener(getContext()) {    //Go créer une méthode qui va s'occuper de l'affichage en fonction de l'aéroport à afficher au lieu de copiuer le code 2 fois
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onSwipeRight() {
+
+                if(airportIterator == 0) airportIterator = lstAirport.size()-1;
+                else airportIterator--;
+
+                test.setText("N°" + airportIterator + " : " + lstAirport.get(airportIterator).getSite());
+
+            }
+
+            @Override
+            public void onSwipeLeft() {
+
+                if(airportIterator == lstAirport.size()-1) airportIterator = 0;
+                else airportIterator++;
+
+                test.setText("N°" + airportIterator + " : " + lstAirport.get(airportIterator).getSite());
+
             }
         });
-        return root;
     }
 
     @Override
