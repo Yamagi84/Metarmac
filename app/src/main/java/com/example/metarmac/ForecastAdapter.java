99c8f116555f2b5a8fc9bb13a781c006b1a90eab
Nasreb <74724888+Nasreb@users.noreply.github.com>
2021-11-10 14:00:25 +0100

    TAF fini v2 + logo

---
+++ app/src/main/java/com/example/metarmac/ForecastAdapter.java
@@ -0,0 +1,103 @@
+package com.example.metarmac;
+
+import android.content.Context;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.metarmac.model.Airport;
+import com.example.metarmac.model.Forecast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ViewHolder> {
+
+    private Context parentContext;
+
+    private ArrayList<Forecast> lstForecast;
+
+    public ForecastAdapter(ArrayList<Forecast> lstForecast) {
+        this.lstForecast = lstForecast;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        parentContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(parentContext);
+
+        View forecastView = inflater.inflate(R.layout.item_forecast, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(forecastView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Forecast forecast = lstForecast.get(position);
+
+        if (position > 0) {
+            TextView taf = holder.taf;
+            taf.setText(parentContext.getResources().getString(R.string.forecast)+ (position+1));
+            taf.setVisibility(View.VISIBLE);
+        }
+        TextView textView = holder.forecast_start;
+        textView.setText(forecast.getFcst_time_from());
+        textView = holder.forecast_end;
+        textView.setText(forecast.getFcst_time_to());
+        textView = holder.forecast_type;
+        textView.setText(forecast.getChange_indicator());
+        textView = holder.wind_direction_taf;
+        textView.setText(forecast.getWind_dir_degrees() + "°");
+        textView = holder.wind_speed_taf;
+        textView.setText(forecast.getWind_speed_kt() + " kts");
+        textView = holder.visibility_taf;
+        textView.setText(forecast.getVisibility_statute_mi() + " mi");
+        textView = holder.clouds_taf;
+        if(forecast.getSky_condition().size() != 0)
+        textView.setText(forecast.getSky_condition().get(0).getSky_cover() + parentContext.getResources().getString(R.string.clouds_at) + forecast.getSky_condition().get(0).getCloud_base_ft_agl() + "m");
+        textView = holder.weather_taf;
+        textView.setText(forecast.getWx_string());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstForecast.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView taf;
+        public TextView forecast_start;
+        public TextView forecast_end;
+        public TextView forecast_type;
+        public TextView wind_direction_taf;
+        public TextView wind_speed_taf;
+        public TextView visibility_taf;
+        public TextView clouds_taf;
+        public TextView weather_taf;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            taf = (TextView) itemView.findViewById(R.id.taf);
+            forecast_start = (TextView) itemView.findViewById(R.id.forecast_start);
+            forecast_end = (TextView) itemView.findViewById(R.id.forecast_end);
+            forecast_type = (TextView) itemView.findViewById(R.id.forecast_type);
+            wind_direction_taf = (TextView) itemView.findViewById(R.id.wind_direction_taf);
+            wind_speed_taf = (TextView) itemView.findViewById(R.id.wind_speed_taf);
+            visibility_taf = (TextView) itemView.findViewById(R.id.visibility_taf);
+            clouds_taf = (TextView) itemView.findViewById(R.id.clouds_taf);
+            weather_taf = (TextView) itemView.findViewById(R.id.weather_taf);
+        }
+    }
+}
--- app/src/main/java/com/example/metarmac/ui/infos/InfosFragment.java
+++ app/src/main/java/com/example/metarmac/ui/infos/InfosFragment.java
@@ -14,7 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.metarmac.ForecastAdapter;
 import com.example.metarmac.GlobalApplication;
 import com.example.metarmac.R;
 import com.example.metarmac.databinding.FragmentInfosBinding;
@@ -48,36 +51,8 @@ public class InfosFragment extends Fragment {
     private TextView pressure;
     private TextView clouds;
 
-    private LinearLayout forecast1;
-    private LinearLayout forecast2;
-    private LinearLayout forecast3;
+
     private TextView taf;
-    private TextView taf2;
-    private TextView taf3;
-    private TextView forecast_start1;
-    private TextView forecast_start2;
-    private TextView forecast_start3;
-    private TextView forecast_end1;
-    private TextView forecast_end2;
-    private TextView forecast_end3;
-    private TextView forecast_type1;
-    private TextView forecast_type2;
-    private TextView forecast_type3;
-    private TextView wind_dir1;
-    private TextView wind_dir2;
-    private TextView wind_dir3;
-    private TextView wind_spd1;
-    private TextView wind_spd2;
-    private TextView wind_spd3;
-    private TextView visibility1;
-    private TextView visibility2;
-    private TextView visibility3;
-    private TextView clouds1;
-    private TextView clouds2;
-    private TextView clouds3;
-    private TextView weather1;
-    private TextView weather2;
-    private TextView weather3;
 
     private TextView current_oaci;
     private Button next_oaci;
@@ -112,38 +87,7 @@ public class InfosFragment extends Fragment {
         pressure = (TextView) root.findViewById(R.id.pressure);
         clouds = (TextView) root.findViewById(R.id.clouds);
 
-        forecast1 = (LinearLayout) root.findViewById(R.id.forecast1);
         taf = (TextView) root.findViewById(R.id.taf);
-        forecast_start1 = (TextView) root.findViewById(R.id.forecast_start1);
-        forecast_end1 = (TextView) root.findViewById(R.id.forecast_end1);
-        forecast_type1 = (TextView) root.findViewById(R.id.forecast_type1);
-        wind_dir1 = (TextView) root.findViewById(R.id.wind_direction_taf1);
-        wind_spd1 = (TextView) root.findViewById(R.id.wind_speed_taf1);
-        visibility1 = (TextView) root.findViewById(R.id.visibility_taf1);
-        clouds1 = (TextView) root.findViewById(R.id.clouds_taf1);
-        weather1 = (TextView) root.findViewById(R.id.weather_taf1);
-
-        forecast2 = (LinearLayout) root.findViewById(R.id.forecast2);
-        taf2 = (TextView) root.findViewById(R.id.taf2);
-        forecast_start2 = (TextView) root.findViewById(R.id.forecast_start2);
-        forecast_end2 = (TextView) root.findViewById(R.id.forecast_end2);
-        forecast_type2 = (TextView) root.findViewById(R.id.forecast_type2);
-        wind_dir2 = (TextView) root.findViewById(R.id.wind_direction_taf2);
-        wind_spd2 = (TextView) root.findViewById(R.id.wind_speed_taf2);
-        visibility2 = (TextView) root.findViewById(R.id.visibility_taf2);
-        clouds2 = (TextView) root.findViewById(R.id.clouds_taf2);
-        weather2 = (TextView) root.findViewById(R.id.weather_taf2);
-
-        forecast3 = (LinearLayout) root.findViewById(R.id.forecast3);
-        taf3 = (TextView) root.findViewById(R.id.taf3);
-        forecast_start3 = (TextView) root.findViewById(R.id.forecast_start3);
-        forecast_end3 = (TextView) root.findViewById(R.id.forecast_end3);
-        forecast_type3 = (TextView) root.findViewById(R.id.forecast_type3);
-        wind_dir3 = (TextView) root.findViewById(R.id.wind_direction_taf3);
-        wind_spd3 = (TextView) root.findViewById(R.id.wind_speed_taf3);
-        visibility3 = (TextView) root.findViewById(R.id.visibility_taf3);
-        clouds3 = (TextView) root.findViewById(R.id.clouds_taf3);
-        weather3 = (TextView) root.findViewById(R.id.weather_taf3);
 
         lstAirport = ((GlobalApplication)getActivity().getApplication()).getLstAirport();
         airportIterator = 0;
@@ -213,8 +157,28 @@ public class InfosFragment extends Fragment {
 
     @SuppressLint("SetTextI18n")
     private void modifyView(Airport airport) {
+        RecyclerView rvForecast = (RecyclerView) getView().findViewById(R.id.forecast_list);
+
+        if(airport.getTaf().getLstForecast() != null) {
+            rvForecast.setVisibility(View.VISIBLE);
+            ForecastAdapter adapter = new ForecastAdapter(airport.getTaf().getLstForecast());
+            rvForecast.setAdapter(adapter);
+
+            rvForecast.setLayoutManager(new LinearLayoutManager(getView().getContext()));
+        }else {
+            rvForecast.setVisibility(View.GONE);
+        }
 
         data.setVisibility(View.VISIBLE);
+
+        if(airport.getTaf().getLstForecast() != null){
+            taf.setBackground(getResources().getDrawable(R.drawable.shape3));
+            taf.setText(airport.getTaf().getRaw_text());
+        } else {
+            taf.setBackground(getResources().getDrawable(R.drawable.shape4));
+            taf.setText(getResources().getString(R.string.no_taf));
+        }
+
 
         if (airport.getMetar().getObservation_time() != null) {
             metar_content.setVisibility(View.VISIBLE);
@@ -234,54 +198,6 @@ public class InfosFragment extends Fragment {
             metar_content.setVisibility(View.GONE);
             metar.setBackground(getResources().getDrawable(R.drawable.shape4));
             metar.setText(getResources().getString(R.string.no_metar));
-        }
-
-        if (airport.getTaf().getLstForecast() != null) {
-            forecast1.setVisibility(View.VISIBLE);
-            taf.setBackground(getResources().getDrawable(R.drawable.shape3));
-            taf.setText(airport.getTaf().getRaw_text());
-            forecast_start1.setText(airport.getTaf().getLstForecast().get(0).getFcst_time_from());
-            forecast_end1.setText(airport.getTaf().getLstForecast().get(0).getFcst_time_to());
-            forecast_type1.setText(airport.getTaf().getLstForecast().get(0).getChange_indicator());
-            wind_dir1.setText(airport.getTaf().getLstForecast().get(0).getWind_dir_degrees() + "°");
-            wind_spd1.setText(airport.getTaf().getLstForecast().get(0).getWind_speed_kt() + " kts");
-            visibility1.setText(airport.getTaf().getLstForecast().get(0).getVisibility_statute_mi() + " mi");
-            if (airport.getTaf().getLstForecast().get(0).getSky_condition().size() != 0)
-            clouds1.setText(airport.getTaf().getLstForecast().get(0).getSky_condition().get(0).getSky_cover()  + getResources().getString(R.string.clouds_at) + airport.getTaf().getLstForecast().get(0).getSky_condition().get(0).getCloud_base_ft_agl());
-            weather1.setText(airport.getTaf().getLstForecast().get(0).getWx_string());
-
-            if (airport.getTaf().getLstForecast().get(1) != null) {
-                taf2.setVisibility(View.VISIBLE);
-                forecast2.setVisibility(View.VISIBLE);
-                forecast_start2.setText(airport.getTaf().getLstForecast().get(1).getFcst_time_from());
-                forecast_end2.setText(airport.getTaf().getLstForecast().get(1).getFcst_time_to());
-                wind_dir2.setText(airport.getTaf().getLstForecast().get(1).getWind_dir_degrees() + "°");
-                wind_spd2.setText(airport.getTaf().getLstForecast().get(1).getWind_speed_kt() + " kts");
-                visibility2.setText(airport.getTaf().getLstForecast().get(1).getVisibility_statute_mi() + " mi");
-                if (airport.getTaf().getLstForecast().get(1).getSky_condition().size() != 0)
-                clouds2.setText(airport.getTaf().getLstForecast().get(1).getSky_condition().get(0).getSky_cover()  + getResources().getString(R.string.clouds_at) + airport.getTaf().getLstForecast().get(1).getSky_condition().get(0).getCloud_base_ft_agl());
-                weather2.setText(airport.getTaf().getLstForecast().get(1).getWx_string());
-            } else { taf2.setVisibility(View.GONE);forecast2.setVisibility(View.GONE);}
-            if (airport.getTaf().getLstForecast().get(2) != null) {
-                taf3.setVisibility(View.VISIBLE);
-                forecast3.setVisibility(View.VISIBLE);
-                forecast_start3.setText(airport.getTaf().getLstForecast().get(2).getFcst_time_from());
-                forecast_end3.setText(airport.getTaf().getLstForecast().get(2).getFcst_time_to());
-                wind_dir3.setText(airport.getTaf().getLstForecast().get(2).getWind_dir_degrees() + "°");
-                wind_spd3.setText(airport.getTaf().getLstForecast().get(2).getWind_speed_kt() + " kts");
-                visibility3.setText(airport.getTaf().getLstForecast().get(2).getVisibility_statute_mi() + " mi");
-                if (airport.getTaf().getLstForecast().get(2).getSky_condition().size() != 0)
-                clouds3.setText(airport.getTaf().getLstForecast().get(2).getSky_condition().get(0).getSky_cover()  + getResources().getString(R.string.clouds_at) + airport.getTaf().getLstForecast().get(2).getSky_condition().get(0).getCloud_base_ft_agl());
-                weather3.setText(airport.getTaf().getLstForecast().get(2).getWx_string());
-            } else {taf3.setVisibility(View.GONE); forecast3.setVisibility(View.GONE);}
-        } else {
-            taf2.setVisibility(View.GONE);
-            taf3.setVisibility(View.GONE);
-            forecast1.setVisibility(View.GONE);
-            forecast2.setVisibility(View.GONE);
-            forecast3.setVisibility(View.GONE);
-            taf.setBackground(getResources().getDrawable(R.drawable.shape4));
-            taf.setText(getResources().getString(R.string.no_taf));
         }
 
         Locale l = new Locale("", airport.getCountry());
